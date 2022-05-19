3a911e3d03fc2e0b5c9cee9d633e93e6404f833f
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-05-19 22:54:49 +0100

    Add a test for the method factorial for when n=4.

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,5 +5,9 @@ describe Solver do
     it 'the method factorial should return 1 when n=0' do
       expect(Solver.new.factorial 0).to eq 1
     end
+
+    it 'the method factorial should return 24 when n=4' do
+      expect(Solver.new.factorial 4).to eq 24
+    end
   end
 end
