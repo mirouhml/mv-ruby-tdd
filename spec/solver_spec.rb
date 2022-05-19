324ffb5e594334c4739dd9245a2f1b5c972b478c
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-05-19 22:59:27 +0100

    Add a test for the method factorial for when n is negative.

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -9,5 +9,9 @@ describe Solver do
     it 'the method factorial should return 24 when n=4' do
       expect(Solver.new.factorial 4).to eq 24
     end
+
+    it 'the method factorial should return a message \'Please enter a positive number.\' when n is negative.' do
+      expect(Solver.new.factorial -3).to eq 'Please enter a positive number.'
+    end
   end
 end
